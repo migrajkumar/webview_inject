8d85b8f2db7444db130b076bd16f3d9515f6548a
Mig Rajkumar <rajkumarmigsingh@gmail.com>
2023-11-16 14:36:11 +0530

    Initial

--- lib/webview_inject.dart
+++ lib/webview_inject.dart
@@ -1,7 +1,29 @@
 library webview_inject;
 
+import 'package:flutter/cupertino.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+
+  void abc() {}
+}
+
+class MyWidget extends StatefulWidget {
+  final String a;
+  const MyWidget({
+    Key? key,
+    required this.a,
+  }) : super(key: key);
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
